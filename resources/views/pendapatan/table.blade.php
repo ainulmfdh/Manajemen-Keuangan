7b3576d2e1ed2f2ef22a1ba33b4d2467b6e1e744
ainul <ainulmufidh@gmail.com>
2025-09-15 10:34:43 +0700

    add responsive mobile

--- resources/views/pendapatan/table.blade.php
+++ resources/views/pendapatan/table.blade.php
@@ -1,4 +1,5 @@
- <table class="w-full">
+<div class="overflow-x-auto">
+  <table class="w-full">
       <thead>
         <tr class="bg-blue-500 text-white">
           <th class="px-6 py-4 text-left font-semibold text-md">No</th>
@@ -25,13 +26,13 @@
                 class="edit-button text-white bg-yellow-400 hover:bg-yellow-500 font-medium rounded-full text-xs px-5 py-2.5"
               >
                 <i class="fas fa-edit mr-1"></i> Edit
-            </button>
-             <button 
+              </button>
+              <button 
                 type="button" 
                 data-id="{{ $pendapatan->id }}" 
                 class="delete-button text-white bg-red-600 hover:bg-red-700 font-medium rounded-full text-xs px-5 py-2.5">
                 <i class="fas fa-trash-alt mr-1"></i> Hapus
-            </button>
+              </button>
             </div>
           </td>
         </tr>
@@ -41,4 +42,5 @@
         </tr>
         @endforelse
       </tbody>
-    </table>
+  </table>
+</div>
